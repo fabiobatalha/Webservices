7cb9f99ea2dad83948bc3dc6b783200ab42421e1
fabiobatalha <fabiobcs@yahoo.com.br>
2011-05-17 17:13:30 -0300

    Changes at joindatabases to include subject content at article documents

--- proc/scielo2couchdb/shs/scielo2couchdb_joindatabases.sh
+++ proc/scielo2couchdb/shs/scielo2couchdb_joindatabases.sh
@@ -2,44 +2,63 @@
 
 # LISTING CONTENT IN the input/iso directory to get the collections and databases available to append in a global database.
 
-databases=`echo "artigo issues title bib4cit"`
-for files in $databases
+#databases=`echo "artigo issues title bib4cit"`
+#for files in $databases
+#do
+echo "--- CREATING EMPTY DATABASES FOR ARTICLE, ISSUE, TITLE and CITATIONS"
+echo "---+--- mkdir -p ../output"
+mkdir -p ../output
+echo "---+--- $cisis_dir/mx null count=0 create=../output/artigo"
+$cisis_dir/mx null count=0 create=../output/artigo
+echo "---+--- $cisis_dir/mx null count=0 create=../output/issues"
+$cisis_dir/mx null count=0 create=../output/issues
+echo "---+--- $cisis_dir/mx null count=0 create=../output/title"
+$cisis_dir/mx null count=0 create=../output/title
+echo "---+--- $cisis_dir/mx null count=0 create=../output/bib4cit"
+$cisis_dir/mx null count=0 create=../output/bib4cit
+echo "--- APPENDING FILES"
+for collection in `ls -1 $database_dir"/input/iso/"`
 do
-echo "Appending $files databases"
-$cisis_dir/mx null count=0 create=org$files
-    for collection in `ls -1 $database_dir"/input/iso/"`
-    do
-       echo $collection
-       file=$database_dir"input/iso/"$collection"/"$files".iso"
-
-       if [ $files == "artigo" ]
-       then
-           echo "proc artigo"
-           proc="proc='a980#$collection#'"
-       elif [ $files == "title" ]
-       then
-           echo "proc title"
-           proc="proc='a706#t#a980#$collection#'" 
-       elif [ $files == "issues" ]
-       then
-           echo "proc issues"
-           proc="proc='a980#$collection#'"
-       elif [ $files == "bib4cit" ]
-       then
-           echo "proc bib4cit"
-           proc="proc='a980#$collection#'"
-       fi
-
-       if [ -f $file ]
-       then
-           $cisis_dir/mx iso=$file $proc append=org$files -all now
-       else
-           echo "[ERROR] file doesnt exist: "$file
-       fi
+   for file in `ls -1 $database_dir"/input/iso/$collection/"`
+   do
+        file_iso=$database_dir"/input/iso/"$collection"/"$file
+        proc=""
+        procb=""
+        if [ $file == "artigo.iso" ]
+        then
+            echo "---+--- artigo.iso from "$collection
+            proc="proc='a980#$collection#'"
+            echo "---+--- INCLUDING JOURNAL DATA INSIDE ARTICLE REGISTER 'SUBJECT'"
+            procb="proc=ref(['../input/$collection/title']l(['../input/$collection/title'],s('ISSN=',v35)),('a966#',v441,'#'))"
+            
+        elif [ $file == "title.iso" ]
+        then
+            echo "---+--- title.iso from "$collection
+            proc="proc='a706#t#a980#$collection#'" 
+        elif [ $file == "issues.iso" ]
+        then
+            echo "---+--- issues.iso from "$collection
+            proc="proc='a980#$collection#'"
+        elif [ $file == "bib4cit.iso" ]
+        then
+            echo "---+--- bib4cit.iso from "$collection
+            proc="proc='a980#$collection#'"
+        fi
+     
+        if [ -f $file_iso ]
+        then
+            echo $cisis_dir/mx iso=$file_iso $proc $procb append=../output/${file/.iso} -all now
+            $cisis_dir/mx iso=$file_iso $proc $procb append=../output/${file/.iso} -all now
+        else
+            echo "---+--- [ERROR] FILE DOESN'T EXISTS: "$file_iso
+        fi
     done
-    $cisis_dir/mx org$files iso=org$files.iso -all now
-    rm org$files.mst org$files.xrf
 done
 
-mkdir -p $database_dir"/output/iso"
-mv *.iso $database_dir"/output/iso"
+for database in `ls -1 ../output/*.mst`
+do
+    echo "---+--- CONVERTING JOINED DATABASE $database TO ISO"
+    $cisis_dir/mx ${database/.mst} iso=${database/.mst}.iso -all now
+done
+rm ../output/*.mst
+rm ../output/*.xrf
--- proc/scielo2couchdb/shs/scielo2couchdb_run.sh
+++ proc/scielo2couchdb/shs/scielo2couchdb_run.sh
@@ -1,7 +1,22 @@
 . scielo2couchdb_config.sh
 
-ECHO "RUNING JOIN DATABASES"
+echo "[CLEANING ENVIRONMENT]"
+echo  "rm -f ../output/iso/*"
+rm -f ../output/iso/*
+
+echo "[DELETING DATABASE $couchdb_database]"
+echo "curl -X DELETE $couchdb_database"
+curl -X DELETE $couchdb_database
+
+echo "[CREATING EMPTY DATABASE $couchdb_database]"
+echo "curl -X PUT $couchdb_database"
+curl -X PUT $couchdb_database
+
+echo "[RUNNIUNG INDEX DATABASES]"
+./scielo2couchdb_index.sh
+
+echo "[RUNING JOIN DATABASES]"
 ./scielo2couchdb_joindatabases.sh
 
-ECHO "RUNNING BULKUP"
+echo "[RUNNING BULKUP]"
 ./scielo2couchdb_bulkup.sh
