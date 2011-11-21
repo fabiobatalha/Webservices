018a26839c18cf6a53f5c39a7e2ad9ca7ccb9085
root <root@localhost.localdomain>
2011-11-21 15:36:17 -0200

    Delete config file

--- proc/scielo2couchdb/shs/scielo2couchdb_config.sh
+++
@@ -1,4 +0,0 @@
-database_dir="/bases/org.000/proc/scielo_webservices/bases/"
-cisis_dir="/usr/local/bireme/cisis/5.5.pre02/linux/lindG4"
-couchdb_database="http://localhost:5984/scieloorg"
-bulk_size="10000"
